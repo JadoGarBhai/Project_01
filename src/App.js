8d6aa5083c3124bad3826b0b11e0ad0999bf23d3
JadoGarBhai <sikandernawaz35@gmail.com>
2023-10-07 23:22:05 +0500

    'Toast'

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import Routing from "./Routing/Routing";
+import AuthContextProvider from "./Context/AuthContext";
+import "react-toastify/dist/ReactToastify.css";
+import { ToastContainer } from "react-toastify";
+import "./Configure/global";
 
 function App() {
   return (
     <>
-      <Routing />
+      <AuthContextProvider>
+        <Routing />
+      </AuthContextProvider>
+
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </>
   );
 }
