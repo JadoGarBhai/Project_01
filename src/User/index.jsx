8e887c2f1320487d33b32b53864765de2f303440
JadoGarBhai <sikandernawaz35@gmail.com>
2023-10-08 00:08:26 +0500

    'getError'

--- src/User/index.jsx
+++ src/User/index.jsx
@@ -3,10 +3,15 @@ import { Route, Routes } from "react-router-dom";
 import Dashboard from "./Dashboard/Dashboard";
 import Login from "./Login/Login";
 import Registration from "./Registration/Registration";
+import PrivateRoute from "../Routing/PrivateRoute";
 
 const index = () => {
   return (
     <Routes>
+      {/* <Route
+        path="/dashboard"
+        element={<PrivateRoute Component={Dashboard} />}
+      /> */}
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/login" element={<Login />} />
       <Route path="/registration" element={<Registration />} />
