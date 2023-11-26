4197a9096d6c0190c5f61bb48ea87583d01a10e3
JadoGarBhai <sikandernawaz35@gmail.com>
2023-11-26 16:45:08 +0500

    'backend'

---
+++ Backend/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+require("dotenv").config();
+
+// create app
+const app = express();
+
+// cors
+app.use(cors());
+app.use(express.json());
+
+// routes
+// const authRoutes = require("./src/routes/userAuthRoutes");
+// app.use("/auth", authRoutes);
+
+// Mongodb connection
+mongoose.connect(process.env.CONNECTION_STRING, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on("error", (error) => {
+  console.log("Mongodb error : ", error);
+});
+
+db.once("open", () => {
+  console.log("Mongodb connected sucessfully!");
+  const port = process.env.PORT;
+  app.listen(port, () => {
+    console.log(`server is running on ${port} port`);
+  });
+});
